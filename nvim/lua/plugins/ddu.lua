04e13995e907c71dd170d921309d51cb6906f028
fujit4 <fujitash4@gmail.com>
2025-04-21 21:45:23 +0900

    ddu

--- nvim/lua/plugins/ddu.lua
+++ nvim/lua/plugins/ddu.lua
@@ -6,22 +6,16 @@ return {
       { "vim-denops/denops.vim", lazy = false },
       { "Shougo/ddu-ui-ff", lazy = false },
       { "Shougo/ddu-source-file_rec", lazy = false },
-      { "Shougo/ddu-source-file_mr", lazy = false },
+      { "Shougo/ddu-source-file_old", lazy = false },
       { "Shougo/ddu-source-register", lazy = false },
       { "Shougo/ddu-source-buffer", lazy = false },
-      { "Shougo/ddu-filter-matcher_substring", lazy = false },
       { "Shougo/ddu-kind-file", lazy = false },
+      { "Shougo/ddu-filter-matcher_substring", lazy = false },
       { "Shougo/ddu-filter-sorter_alpha" },
     },
     config = function()
       vim.fn["ddu#custom#patch_global"]({
         ui = "ff",
-        sources = {
-          { name = "file_rec" },
-	  { name = "file_mr", params = { kind = "file" } },
-	  { name = "register" },
-	  { name = "buffer" },
-        },
         sourceOptions = {
           _ = {
             matchers = { "matcher_substring" },
@@ -35,23 +29,42 @@ return {
         },
       })
 
-      -- <leader>fでファイルリストを開く
-      vim.api.nvim_set_keymap("n", "<leader>f", "<Cmd>call ddu#start({})<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<leader>fb", "<Cmd>call ddu#start({ 'sources': [{'name': 'buffer'}] })<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<leader>fr", "<Cmd>call ddu#start({ 'sources': [{'name': 'register'}] })<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<leader>fm", "<Cmd>call ddu#start({ 'sources': [{'name': 'file_mr', 'params': {'kind': 'file'}}] })<CR>", { noremap = true, silent = true })
+      -- <leader>f: 全ファイル検索
+      vim.api.nvim_set_keymap("n", "<leader>f", "<Cmd>call ddu#start({ 'sources': [{ 'name': 'file_rec' }] })<CR>", { noremap = true, silent = true })
 
-      -- <CR>でファイルオープン
+      -- <leader>m: 最近使ったファイル（mr）
+      vim.api.nvim_set_keymap("n", "<leader>o", "<Cmd>call ddu#start({ 'sources': [{ 'name': 'file_old' }] })<CR>", { noremap = true, silent = true })
+
+      -- <leader>b: バッファ一覧
+      vim.api.nvim_set_keymap("n", "<leader>b", "<Cmd>call ddu#start({ 'sources': [{ 'name': 'buffer' }] })<CR>", { noremap = true, silent = true })
+
+      -- <leader>r: レジスタ
+      vim.api.nvim_set_keymap("n", "<leader>r", "<Cmd>call ddu#start({ 'sources': [{ 'name': 'register' }] })<CR>", { noremap = true, silent = true })
+
       vim.api.nvim_create_autocmd("FileType", {
 	      pattern = "ddu-ff",
 	      callback = function()
+		      -- <CR>でファイルオープン
 		      vim.keymap.set("n", "<CR>", function()
 			      vim.fn["ddu#ui#do_action"]("itemAction")
 		      end, { buffer = true })
 
+		      -- qでクローズ
 		      vim.keymap.set("n", "q", function()
 			      vim.fn["ddu#ui#do_action"]("quit")
 		      end, { buffer = true })
+
+		      -- sでシステムオープン
+		      vim.keymap.set("n", "s", function()
+			      local items = vim.fn["ddu#ui#get_items"]()
+			      local index = vim.fn.line(".")
+			      local item = items[index]
+			      if item ~= nil then
+				      local path = item.word
+				      vim.fn.jobstart({ "cmd.exe", "/c", "start", "", path }, { detach = true })
+			      end
+		      end, { buffer = true })
+
 	      end,
       })
     end,
